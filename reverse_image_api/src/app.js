00dab93c62330962c40898753705d7917a1b79da
Shinsekami <shinsekami@gmail.com>
2025-06-08 00:24:45 +0200

    Update server port configuration to default to 8080

--- reverse_image_api/src/app.js
+++ reverse_image_api/src/app.js
@@ -8,7 +8,7 @@ const routes = require('./routes');
 const app = express();
 
 // Environment variables
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const ENV = process.env.NODE_ENV || 'development';
 
 // CORS configuration
